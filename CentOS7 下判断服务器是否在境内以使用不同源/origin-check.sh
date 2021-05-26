fe54ed4f201b94ed464835900c444211d0ab5767
senjianlu <admin@cs.cheap>
2021-05-26 17:53:52 +0800

    修改服务器位置判断和使用源脚本

--- CentOS7 下判断服务器是否在境内以使用不同源/origin-check.sh
+++ CentOS7 下判断服务器是否在境内以使用不同源/origin-check.sh
@@ -4,10 +4,10 @@
 ip_info=$(curl http://ip-api.com/json/?lang=zh-CN)
 if [[ ${ip_info} == *"中国"* ]];
 then
-    echo "当前服务器在境内，使用源：Gitee！"
+    # echo "当前服务器在境内，使用源：Gitee！"
     origin="gitee"
 else
-    echo "当前服务器在境外，使用源：GitHub！"
+    # echo "当前服务器在境外，使用源：GitHub！"
     origin="github"
 fi
-exit $origin
+echo $origin
--- 临时开发测试用/test.sh
+++ 临时开发测试用/test.sh
@@ -5,3 +5,5 @@
 # echo $work_path
 
 # 判断服务器在国内还是国外来使用不同源
+sh_content=$(curl -s https://raw.githubusercontent.com/senjianlu/one-click-scripts/main/CentOS7%20%E4%B8%8B%E5%88%A4%E6%96%AD%E6%9C%8D%E5%8A%A1%E5%99%A8%E6%98%AF%E5%90%A6%E5%9C%A8%E5%A2%83%E5%86%85%E4%BB%A5%E4%BD%BF%E7%94%A8%E4%B8%8D%E5%90%8C%E6%BA%90/origin-check.sh | bash)
+echo $sh_content
