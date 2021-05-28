5ffda7e5f745019e9d1869cd2a11e1cfb2fdfb25
senjianlu <admin@cs.cheap>
2021-05-28 10:55:41 +0800

    Linode CentOS7 服务器测试通过一键安装 SOCKS5 脚本

--- CentOS7 下一键安装 SOCKS5 代理/install.sh
+++ CentOS7 下一键安装 SOCKS5 代理/install.sh
@@ -16,12 +16,14 @@ origin=$(curl -s https://gitee.com/senjianlu/one-click-scripts/raw/main/CentOS7%
 opened_port=$(curl -s https://gitee.com/senjianlu/one-click-scripts/raw/main/CentOS7%20%E4%B8%8B%E4%B8%80%E9%94%AE%E5%BC%80%E5%90%AF%E9%98%B2%E7%81%AB%E5%A2%99%E6%8C%87%E5%AE%9A%E7%AB%AF%E5%8F%A3/open-port.sh | bash -s $ss5_port)
 
 # 出现错误立即终止脚本
-set -e
+# set -e
 
 # 默认安装路径
 install_path=/usr/local/share
 
 # 安装环境包
+yum -y install openssl-devel
+yum -y install gcc
 yum -y install pam-devel
 yum -y install openldap-devel
 # 下载、解压和安装
@@ -31,21 +33,26 @@ tar -zxvf ss5-3.8.9-8.tar.gz
 mv ss5-3.8.9 $install_path
 cd $install_path/ss5-3.8.9
 ./configure && make && make install
+# 新建 /var/run/ss5/ss5.pid 用文件夹
+mkdir /var/run/ss5
+# 开机自动新建文件夹防止 SOCKS5 开机启动出错
+echo "@reboot mkdir -p /var/run/ss5" >> /var/spool/cron/root
+service crond restart
 
 # 回到脚本所在路径
 cd $work_path
 # 开启认证
 sed -i "87c auth    0.0.0.0/0               -              u" /etc/opt/ss5/ss5.conf
 sed -i "203c permit  u       0.0.0.0/0       -       0.0.0.0/0       -       -       -       -       -" /etc/opt/ss5/ss5.conf
-# 更改监听端口
-sed -i "7c export SS5_SOCKS_PORT=$ss5_port" /etc/rc.d/init.d/ss5
-sed -i "8c export SS5_SOCKS_USER=root" /etc/rc.d/init.d/ss5
 # 新增用户
 sed -i "1c $ss5_username $ss5_password" /etc/opt/ss5/ss5.passwd
 # 修正 /etc/rc.d/init.d/ss5 文件语法错误
 sed -i '18c  [[ ${NETWORKING} = "no" ]] && exit 0' /etc/rc.d/init.d/ss5
+# 更改监听端口
+sed -i "N;8iexport SS5_SOCKS_PORT=$ss5_port" /etc/rc.d/init.d/ss5
+sed -i 'N;7aexport SS5_SOCKS_USER=root' /etc/rc.d/init.d/ss5
 
-# 设置开机自启
+# 开启 SOCKS5 的开机自启动服务
 chmod u+x /etc/rc.d/init.d/ss5
 chkconfig --add ss5
 chkconfig ss5 on
