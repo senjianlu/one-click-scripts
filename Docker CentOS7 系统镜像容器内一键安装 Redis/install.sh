5578ed3df60caf5e0574731a6635f7a0e0973c19
Rabbir <m18018230731@163.com>
2021-10-19 10:09:52 +0800

    新增 Docker CentOS7 系统镜像容器内一键安装 Redis 脚本

---
+++ Docker CentOS7 系统镜像容器内一键安装 Redis/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 作者：Rabbir
+
+# 判断服务器是否在境内以使用不同源
+origin=$(curl -s https://gitee.com/senjianlu/one-click-scripts/raw/main/CentOS7%20%E4%B8%8B%E5%88%A4%E6%96%AD%E6%9C%8D%E5%8A%A1%E5%99%A8%E6%98%AF%E5%90%A6%E5%9C%A8%E5%A2%83%E5%86%85%E4%BB%A5%E4%BD%BF%E7%94%A8%E4%B8%8D%E5%90%8C%E6%BA%90/origin-check.sh | bash)
+
+# 下载 Redis 安装包
+redis_tar_gz_url=https://$origin.com/senjianlu/one-click-scripts/raw/main/mirror/Redis/redis-6.2.6.tar.gz
+if [[ ${origin} == *"github"* ]];
+then
+    wget $redis_tar_gz_url
+else
+    curl -s https://gitee.com/senjianlu/one-click-scripts/raw/main/CentOS7%20%E4%B8%8B%E4%B8%8B%E8%BD%BD%20Gitee%20%E8%B6%85%E8%BF%87%201%20MB%20%E5%A4%A7%E5%B0%8F%E7%9A%84%E6%96%87%E4%BB%B6/download.sh | bash -s $redis_tar_gz_url
+fi
+
+# 安装 Redis
+tar xzf redis-6.2.6.tar.gz
+cd redis-6.2.6
+make && make install
+
+# 删除压缩包
+cd ..
+rm -rf redis-6.2.6.tar.gz
