a2047968f38c767c419d1c1de31355182852165c
Rabbir <admin@cs.cheap>
2021-11-13 10:16:19 +0800

    fix: 修于 ip-api.com 不稳定而返回空值的情况

--- CentOS7 下判断服务器是否在境内以使用不同源/origin-check.sh
+++ CentOS7 下判断服务器是否在境内以使用不同源/origin-check.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# 默认服务器在境外
+origin="github"
+
 # 判断服务器在国内还是国外来使用不同源
-ip_info=$(curl http://ip-api.com/json/?lang=zh-CN)
+ip_info=$(curl 'https://www.ip.cn/api/index?ip=&type=0')
 if [[ ${ip_info} == *"中国"* ]];
 then
     # echo "当前服务器在境内，使用源：Gitee！"
