de972f74514fef3dc2a9b473f3531bb3090f2253
Rabbir <m18018230731@163.com>
2022-01-12 13:57:57 +0800

    fix: 修复 CentOS7 下一键安装 Clash 客户端脚本的错误

--- CentOS7 下一键安装 Clash 客户端/install.sh
+++ CentOS7 下一键安装 Clash 客户端/install.sh
@@ -15,10 +15,10 @@ then
 else
     curl -s https://gitee.com/senjianlu/one-click-scripts/raw/main/CentOS7%20%E4%B8%8B%E4%B8%8B%E8%BD%BD%20Gitee%20%E8%B6%85%E8%BF%87%201%20MB%20%E5%A4%A7%E5%B0%8F%E7%9A%84%E6%96%87%E4%BB%B6/download.sh | bash -s $clash_gz_url
 fi
-gunzip clash-linux-amd64-v1.7.1.gz
+gunzip clash-linux-amd64-v1.2.0.gz
 
 # 转移到 /usr/bin 使其在任意目录都可执行
-mv clash-linux-amd64-v1.7.1 /usr/local/bin/clash
+mv clash-linux-amd64-v1.2.0 /usr/local/bin/clash
 chmod 777 /usr/local/bin/clash
 
 # 安装完成
