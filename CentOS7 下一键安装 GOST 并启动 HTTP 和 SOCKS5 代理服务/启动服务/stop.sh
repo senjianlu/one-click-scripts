c8cc0374d3cd73d7c2e1a8a48d045b86bf0d9c10
Rabbir <m18018230731@163.com>
2022-02-28 11:04:44 +0800

    feat: 新增对指定端口的 GOST 进行关闭的功能

--- CentOS7 下一键安装 GOST 并启动 HTTP 和 SOCKS5 代理服务/启动服务/stop.sh
+++ CentOS7 下一键安装 GOST 并启动 HTTP 和 SOCKS5 代理服务/启动服务/stop.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # gost stop
 
-kill -9 $(pgrep -f 'gost')
+# GOST 本地转发端口
+local_port=$1
+
+# 如果指定了 GOST 转发的本地端口
+if [ $local_port ]; then
+    kill -9 $(pgrep -f "$local_port -F")
+else
+    kill -9 $(pgrep -f 'gost')
+fi
